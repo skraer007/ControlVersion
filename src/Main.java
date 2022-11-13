0fb9db8426e05af78c0cbdab58586de02e5829d2
Andrey Ivanov <skraer2001@mail.ru>
2022-11-13 16:54:12 +0300

    Final1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = {99,30,5,100,78};
+            SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
     }
 }
