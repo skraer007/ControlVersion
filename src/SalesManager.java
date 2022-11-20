90df69e1fb0156637f72dd564d8e7ea3560e161d
Andrey Ivanov <skraer2001@mail.ru>
2022-11-20 18:24:36 +0300

    Add average method

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,25 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int average(){
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        int max = max();
+        int amount = 0;
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale == max | sale == min) {
+                continue;
+            } else {
+                sum += sale;
+                amount++;
+            }
+        }
+        return sum/amount;
+    }
 }
